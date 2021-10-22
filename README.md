b1c8bc76c12eea2174a961603b2bcd9c0debcdd3
byWulf <wulf1337@gmail.com>
2021-10-22 18:16:46 +0200

    Add better error handling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jigsawlutioner-controller-placer
+
+http://192.168.0.107:3000/reset?pushMotor[port]=C&moveMotor[port]=B&boardMotor[port]=A&plateMotor[port]=D
--- index.js
+++ index.js
@@ -4,53 +4,62 @@ import BrickPi from 'brickpi3';
 const controller = new Controller(3000);
 
 controller.createEndpoint('reset', async (parameters, resolve) => {
-    const pushMotor = await controller.getMotor(parameters.pushMotor);
-    const moveMotor = await controller.getMotor(parameters.moveMotor);
-    const boardMotor = await controller.getMotor(parameters.boardMotor);
-    const plateMotor = await controller.getMotor(parameters.plateMotor);
+    const pushMotor = await controller.getMotor(parameters, 'pushMotor');
+    const moveMotor = await controller.getMotor(parameters, 'moveMotor');
+    const boardMotor = await controller.getMotor(parameters, 'boardMotor');
+    const plateMotor = await controller.getMotor(parameters, 'plateMotor');
+
+    await controller.resetMotor(pushMotor, BrickPi.utils.RESET_MOTOR_LIMIT.FORWARD_LIMIT, 30);
 
     await Promise.all([
-        controller.resetMotor(pushMotor, BrickPi.utils.RESET_MOTOR_LIMIT.FORWARD_LIMIT, 30),
         controller.resetMotor(moveMotor, BrickPi.utils.RESET_MOTOR_LIMIT.BACKWARD_LIMIT, 50),
-        controller.resetMotor(boardMotor, BrickPi.utils.RESET_MOTOR_LIMIT.FORWARD_LIMIT, 70),
-        controller.resetMotor(plateMotor, BrickPi.utils.RESET_MOTOR_LIMIT.FORWARD_LIMIT, 50),
+        controller.resetMotor(boardMotor, BrickPi.utils.RESET_MOTOR_LIMIT.FORWARD_LIMIT, 80),
+        controller.resetMotor(plateMotor, BrickPi.utils.RESET_MOTOR_LIMIT.BACKWARD_LIMIT, 50),
     ]);
 });
 
 controller.createEndpoint('place', async (parameters, resolve) => {
-    const pushMotor = await controller.getMotor(parameters.pushMotor);
-    const moveMotor = await controller.getMotor(parameters.moveMotor);
-    const boardMotor = await controller.getMotor(parameters.boardMotor);
-    const plateMotor = await controller.getMotor(parameters.plateMotor);
+    const pushMotor = await controller.getMotor(parameters, 'pushMotor');
+    const moveMotor = await controller.getMotor(parameters, 'moveMotor');
+    const boardMotor = await controller.getMotor(parameters, 'boardMotor');
+    const plateMotor = await controller.getMotor(parameters, 'plateMotor');
+
+    const targetPlatePosition = Math.random() * 900;
 
     await Promise.all([
         (async () => {
             await Promise.all([
-                movePieceToCliff(moveMotor, pushMotor, parameters.offset),
+                (async() => {
+                    await movePieceToCliff(moveMotor, pushMotor, parameters.offset || 0);
+
+                    resolve();
+                })(),
                 plateMotor.setPosition(0),
             ]);
-            await Promise.all([
-                moveMotor.setPosition(900),
-                pushMotor.setPosition(-200),
-            ]);
 
-
-            resolve();
+            await pushMotor.setPosition(-170);
+            await moveMotor.setPosition(850);
 
             await Promise.all([
-                moveMotor.setPosition(1500),
-                plateMotor.setPosition(-670)
+                moveMotor.setPosition(850 + targetPlatePosition),
+                plateMotor.setPosition(targetPlatePosition)
             ]);
         })(),
         boardMotor.setPosition(Math.random() * -1000),
     ]);
 
-    await plateMotor.setPosition(0);
+    await plateMotor.setPosition(Math.max(targetPlatePosition + 400, 0));
+    await pushMotor.setPosition(0);
 
     await Promise.all([
-        pushMotor.setPosition(0),
+        plateMotor.setPosition(0),
         moveMotor.setPosition(500),
     ]);
+
+    pushMotor.setPower(0);
+    moveMotor.setPower(0);
+    boardMotor.setPower(0);
+    plateMotor.setPower(0);
 });
 
 async function movePieceToCliff(moveMotor, pushMotor, offset) {
@@ -62,7 +71,7 @@ async function movePieceToCliff(moveMotor, pushMotor, offset) {
 
     await moveMotor.setPosition(-offsetInDegree);
 
-    await pushMotor.setPosition(-150);
+    await pushMotor.setPosition(-100);
 
-    await moveMotor.setPosition(500);
+    await moveMotor.setPosition(630);
 }
